0cfbb9853a352039ca8f4ae505760844093c5a19
ted-thuraa <mutwirited@gmail.com>
2022-11-18 08:28:03 +0300

    up

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { useEffect } from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { FiSettings } from 'react-icons/fi';
+import { TooltipCompoonent } from '@syncfusion/ej2-react-popups';
+
+import './App.css';
+const App = () => {
+  return (
+    <div>
+        <h1 className="underline text-3xl">App</h1>
+    </div>
+  )
+}
+
+export default App
